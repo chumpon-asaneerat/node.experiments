4c29f78ae5f494c00d4acad31dc3ef2986c71d3b
chumpon-asaneerat <chumponsenate@gmail.com>
2018-07-29 14:56:22 +0700

    add api route.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const app = express();
 
+app.get('/api', function(req, res) {
+    res.json({
+        text: 'my api'
+    });
+});
+
 app.listen(3000, function() {
     console.log('App listening on port 3000.');
 });
